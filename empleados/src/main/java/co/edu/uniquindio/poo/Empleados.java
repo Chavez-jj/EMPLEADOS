a58108e499f05898c8e9495db9dfe91d0a97f70c
jharpin <154296932+jharpin@users.noreply.github.com>
2024-10-07 12:10:32 -0500

    implementacion de la clase abstracta  empleado

---
+++ empleados/src/main/java/co/edu/uniquindio/poo/Empleados.java
@@ -0,0 +1,56 @@
+package co.edu.uniquindio.poo;
+
+public abstract class Empleados {
+    private String nombre; 
+    private int Id; 
+    private int salario;
+
+
+/**
+ * metodo contructor de la clase empleado
+ * @param nombre
+ * @param Id
+ */
+    public Empleados(String nombre,int Id,int salario){
+        this.nombre=nombre;
+        this.Id=Id;
+        this.salario=salario;
+    }
+/**
+ * 
+ * Metodo abtracto para calcular el salario
+ */
+    public abstract int CalcularSalario();
+
+/**
+ * 
+ * Metodos getters y setters 
+ */
+public String getNombre() {
+    return nombre;
+}
+
+
+public void setNombre(String nombre) {
+    this.nombre = nombre;
+}
+
+
+public int getId() {
+    return Id;
+}
+
+
+public void setId(int id) {
+   this.Id = id;
+}
+
+public int getsalario(){
+    return salario;
+}
+
+public void setsalario(int salario){
+    this.salario=salario;
+}
+}
+ 
