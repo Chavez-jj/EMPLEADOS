383660a73ae8263ff3d71d48a97a64dd43c6c392
jharpin <154296932+jharpin@users.noreply.github.com>
2024-10-09 10:36:22 -0500

    cambios

--- empleados/src/main/java/co/edu/uniquindio/poo/App.java
+++ empleados/src/main/java/co/edu/uniquindio/poo/App.java
@@ -6,7 +6,7 @@ package co.edu.uniquindio.poo;
  */
 public class App {
     public static void main(String[] args) {
-        EmpleadoTiempoCompleto empleado = new EmpleadoTiempoCompleto("Juan", 123, 2000);
+        EmpleadoTiempoCompleto empleado = new EmpleadoTiempoCompleto("valery", 123, 2000);
         System.out.println(empleado.toString());  
     }
 }
