43d55a61e3953dc4d31473632a8476442679abb2
jharpin <154296932+jharpin@users.noreply.github.com>
2024-10-10 12:04:47 -0500

    implemetacion del tdd de la clase empleadoporhora

--- empleados/src/main/java/co/edu/uniquindio/poo/EmpleadoPorHora.java
+++ empleados/src/main/java/co/edu/uniquindio/poo/EmpleadoPorHora.java
@@ -7,16 +7,24 @@ public class EmpleadoPorHora extends Empleado {
 
     public EmpleadoPorHora(String nombre, int Id,int horasTrabajadas, int salarioPorHora){
         super(nombre,Id);
+        validarIdNegativo(Id);
         this.horasTrabajadas=horasTrabajadas;
         this.salarioPorHora=salarioPorHora;
     }
+    /**
+     * Método que valida si el ID es negativo
+     * @param Id
+     */
+    public void validarIdNegativo(int Id) {
+        if (Id < 0) {
+            throw new IllegalArgumentException("El ID no puede ser negativo.");
+        }
+    }
+
     @Override
  public int calcularSalario(){
     int pagoTotal = 0;
     pagoTotal = horasTrabajadas * salarioPorHora;
-
-
-
 
     return pagoTotal;
 }
